01d72be814e735898e96b9d4facb8e41ca3ec84b
Pavelkv94 <pavel.kv94@yandex.by>
2020-10-12 02:32:17 +0300

    init: start and complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  //проверка на четность длины
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+    //преобразим str В массив
+    let arr = str.split("");
+    //Сравним элементы str и конфига
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+          //если в массиве рядом стоят парные скобки то убираем их
+            if (
+                arr[i] === bracketsConfig[j][0] &&
+                arr[i + 1] === bracketsConfig[j][1]
+            ) {
+                arr.splice(i, 2);
+                i -= 2;
+            }
+        }
+    }
+    //смотрим, остались в массиве элементы или он пустой
+    return arr.length === 0;
+};
